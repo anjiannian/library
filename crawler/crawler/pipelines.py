7e4c8e33cd740265f4fc4640a05416f1febe01d1
Chad Ying <anjiannian@gmail.com>
2015-04-30 10:36:20 +0800

    feat: update settings and pipline for crawler

--- crawler/crawler/pipelines.py
+++ crawler/crawler/pipelines.py
@@ -5,7 +5,53 @@
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: http://doc.scrapy.org/en/latest/topics/item-pipeline.html
 
+from datetime import datetime
+
+import MySQLdb
+from scrapy import log
+
 
 class CrawlerPipeline(object):
     def process_item(self, item, spider):
+        return item
+
+
+class MySQLPipeline(object):
+
+    def __init(self):
+        self.conn = MySQLdb.connect(
+            user='nobody', db='dbname', host='127.0.0.1',
+            charset="utf8", use_unicode=True)
+        self.cursor = self.conn.cursor()
+
+    def process_item(self, item, spider):
+        create_time = datetime.now()
+        try:
+            self.cursor.execute(
+                """
+                INSERT INTO book (title, subtitle, title, subtitle,
+                    origin_title, author, translator, publisher, pubdate,
+                    pages, price, summary, series, binding, isbn10, isbn13,
+                    created) VALUES (
+                %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
+                """, (item['title'].encode('utf-8'),
+                      item['subtitle'].encode('utf-8'),
+                      item['origin_title'].encode('utf-8'),
+                      item['author'].encode('utf-8'),
+                      item['translator'].encode('utf-8'),
+                      item['publisher'].encode('utf-8'),
+                      item['pubdate'],
+                      item['pages'],
+                      item['price'],
+                      item['summary'].encode('utf-8'),
+                      item['series'].encode('utf-8'),
+                      item['binding'].encode('utf-8'),
+                      item['isbn10'],
+                      item['isbn13'], create_time))
+            self.conn.commit()
+            log.msg("Sucess: %s, %s" % (item['isbn13'], item['title']),
+                    level=log.INFO)
+        except MySQLdb.Error, e:
+            log.msg("Error: %d: %s" % (e.args[0], e.args[1]), level=log.ERROR)
+
         return item
--- crawler/crawler/settings.py
+++ crawler/crawler/settings.py
@@ -13,5 +13,22 @@ BOT_NAME = 'crawler'
 SPIDER_MODULES = ['crawler.spiders']
 NEWSPIDER_MODULE = 'crawler.spiders'
 
-# Crawl responsibly by identifying yourself (and your website) on the user-agent
-#USER_AGENT = 'crawler (+http://www.yourdomain.com)'
+ITEM_PIPELINES = {
+    'crawler.pipelines.MySQLdb': 301,
+}
+
+DOWNLOAD_DELAY = 5
+RANDOMIZE_DOWNLOAD_DELAY = True
+
+
+USER_AGENT = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_3) AppleWebKit/536.5 \
+     (KHTML, like Gecko) Chrome/19.0.1084.54 Safari/536.5'
+
+DEFAULT_REQUEST_HEADERS = {
+    "Accept:text/html": "application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
+    "Accept-Encoding": "gzip,deflate,sdch",
+    "Accept-Language": "zh-CN,zh;q=0.8,en;q=0.6,zh-TW;q=0.4",
+    "Cache-Control": "max-age=0",
+    "Connection": "keep-alive",
+    "X-Requested-With": "XMLHttpRequest",
+    "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Ubuntu Chromium/36.0.1985.125 Chrome/36.0.1985.125 Safari/537.36"}
