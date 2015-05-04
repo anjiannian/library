2065ce04235d4a14923085a037e9aec611b1603e
Chad Ying <anjiannian@gmail.com>
2015-05-04 14:02:29 +0800

    fix: fix some proxy issue

--- crawler/crawler/middlewares.py
+++ crawler/crawler/middlewares.py
@@ -16,20 +16,23 @@ proxy_file = os.path.join(PATH, 'proxies.txt')
 class ProxyMiddleware(object):
 
     def __init__(self):
-        log.msg("++++++++++++++++++++++", level=log.DEBUG)
         self.proxies = get_proxy()
 
     def process_request(self, request, spider):
         if 'proxy' in request.meta:
             return
-        request.meta['proxy'] = random.choice(self.proxies)
+        while not 'proxy':
+            self.proxies = get_proxy()
+        request.meta['proxy'] = 'http://' + random.choice(self.proxies)
+        log.msg("----|%s|||%s|----" % (request.meta['proxy'], request.url),
+                level=log.DEBUG)
 
     def process_exception(self, request, exception, spider):
         proxy = request.meta['proxy']
         log.msg('Removing failed proxy <%s>, %d proxies left' % (
-            proxy, len(self.proxies)))
+            proxy, len(self.proxies)), level=log.INFO)
         try:
-            self.proxies.remove(proxy)
+            self.proxies.remove(proxy[7:])
         except ValueError:
             pass
 
@@ -40,11 +43,11 @@ def is_bad_proxy(proxy):
         opener = urllib2.build_opener(proxy_handler)
         opener.addheaders = [('User-agent', 'Mozilla/5.0')]
         urllib2.install_opener(opener)
-        req = urllib2.Request('http://www.baidu.com')
+        req = urllib2.Request('http://www.douban.com')
         urllib2.urlopen(req)
     except urllib2.HTTPError, e:
         print 'Error code: ', e.code
-        return e.code
+        return True
     except Exception, detail:
         print "ERROR:", detail
         return True
@@ -52,7 +55,7 @@ def is_bad_proxy(proxy):
 
 
 def get_proxy():
-    socket.setdefaulttimeout(5)
+    socket.setdefaulttimeout(3)
     resp = requests.get("http://cn-proxy.com/")
     root = html.fromstring(resp.content)
     xpath = "//table[@class='sortable']/tbody/tr/td[position()=1 or position()=2]"
@@ -63,15 +66,18 @@ def get_proxy():
         proxy = res[i] + ':' + res[i+1]
         proxies.append(proxy)
     with open(proxy_file, 'r') as input:
-        proxies.extend(list(input))
-    proxies = list(set(proxies))
+        for line in input:
+            proxy = line.strip()
+            if proxy not in proxies:
+                proxies.append(proxy)
+    result = []
     for proxy in proxies:
-        if is_bad_proxy(proxy):
-            proxies.remove(proxy)
+        if not is_bad_proxy(proxy):
+            result.append(proxy)
     with open(proxy_file, 'w') as output:
-        for proxy in proxies:
+        for proxy in result:
             output.write(proxy + '\n')
-    return proxies
+    return result
 
 if __name__ == '__main__':
     get_proxy()
--- crawler/crawler/settings.py
+++ crawler/crawler/settings.py
@@ -13,13 +13,21 @@ BOT_NAME = 'crawler'
 SPIDER_MODULES = ['crawler.spiders']
 NEWSPIDER_MODULE = 'crawler.spiders'
 
+# Retry many times since proxy fails a lot
+# RETRY_TIMES = 10
+# RETRY_HTTP_CODES = [500, 503, 504, 400, 403, 404, 408]
+
+# DOWNLOADER_MIDDLEWARES = {
+#     'scrapy.contrib.downloadermiddleware.retry.RetryMiddleware': 90,
+#     'crawler.middlewares.ProxyMiddleware': 101,
+# }
+
 ITEM_PIPELINES = {
     'crawler.pipelines.MySQLPipeline': 301,
-    'crawler.middlewares.ProxyMiddleware': 101,
 }
 
-# DOWNLOAD_DELAY = 3
-# RANDOMIZE_DOWNLOAD_DELAY = True
+DOWNLOAD_DELAY = 3
+RANDOMIZE_DOWNLOAD_DELAY = True
 
 
 USER_AGENT = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_3) AppleWebKit/536.5 \
