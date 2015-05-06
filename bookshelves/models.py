9c18b53af7c63ffe783038067e2e5fddb10f7d80
Chad Ying <anjiannian@gmail.com>
2015-05-07 01:26:48 +0800

    feat: add bookshelf model

--- bookshelves/models.py
+++ bookshelves/models.py
@@ -1,3 +1,11 @@
+# -*- coding: utf-8 -*-
+
 from django.db import models
 
-# Create your models here.
+from school.models import School
+
+
+class BookShelf(models.Model):
+    name = models.CharField(max_length=20)
+    school = models.ForeignKey(School)
+    extra = models.TextField(null=True)
